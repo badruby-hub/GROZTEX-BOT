564827dfdb9fffe264b926aabb1c6286e9a9aad1
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-11 01:06:23 +0300

    правки в курсе валют

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ bot.on("message", async (msg) => {
       const apiBuy = parseFloat(data.buy); 
       const apiSell = parseFloat(data.sell);
       const buy = (apiBuy + 0.5).toFixed(2);
-      const sell = (apiSell + 0.5).toFixed(2);
+      const sell = (apiSell - 0.5).toFixed(2);
       await bot.sendMessage(
    chatId, `
 Курс обмена рублей на USDT:
