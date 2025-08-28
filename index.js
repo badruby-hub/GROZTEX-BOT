92a058ddeab20e18d3b94bed54610e172fa8b6f9
Nazim Fataliev <nazim.rabota@bk.ru>
2025-08-28 18:18:26 +0300

    correct valuation

--- index.js
+++ index.js
@@ -79,8 +79,8 @@ bot.on("message", async (msg) => {
       const data = await response.json();
       const apiBuy = parseFloat(data.buy); 
       const apiSell = parseFloat(data.sell);
-      const buy = (apiBuy - 0.5).toFixed(2);
-      const sell = (apiSell + 0.5).toFixed(2);
+      const buy = (apiBuy - 0.4).toFixed(2);
+      const sell = (apiSell + 0.4).toFixed(2);
 
 
       await bot.sendMessage(
